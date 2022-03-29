fbd2d16e44e87cf25fe03d6f514440d6fc05e944
WirWollen <riddeneez@gmail.com>
2022-03-30 02:06:44 +0400

    101

---
+++ Assets/CapsuleExampleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CapsuleExampleController : MonoBehaviour
+{
+    [SerializeField] private List<Transform> goals;
+    [SerializeField] private float minDistance;
+
+    private NavMeshAgent _agent;
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.destination = goals[Random.Range(0, goals.Count - 1)].position;
+    }
+
+    private void Update()
+    {
+        
+        if (_agent.destination!=null && Vector3.Distance(_agent.destination, transform.position) < minDistance)
+        {
+            while (true)
+            {
+                var g = goals[Random.Range(0, goals.Count - 1)].position;
+                if (g!=_agent.destination)
+                {
+                    _agent.destination = g;
+                    break;
+                }
+            }
+        }
+    }
+}
